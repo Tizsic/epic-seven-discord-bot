b78708f17f6ff77525cc377acc4b1747747a7af5
Stefan <itsrhyett@gmail.com>
2023-02-17 04:45:32 -0400

    Commands Setup
    Setting up commands for the bot

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Require necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require ('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require ('discord.js');
 const { token } = require ('./config.json');
-
+const fs = require('node:fs');
+const path = require('node:path');
 // Creates a new client instance
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -13,3 +14,25 @@ client.once(Events.ClientReady, c => {
 
 // Login to Discord with your client's token
 client.login(token);
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, interaction => {
+	console.log(interaction);
+});
